5cd02199212c9018e21153c921891855bdc0787e
Ezequiel <martinez.ezequiel42@gmail.com>
2023-05-05 00:15:51 -0700

    created player inputs and added test functions to the Player script

---
+++ top-down dungeon crawler/Assets/Scripts/PlayerScripts/PlayerManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField]
+    Player player;
+
+    Inputs inputs;
+
+    void Awake()
+    {
+        inputs = new Inputs();
+        inputs.Enable();
+        inputs.Movement.North.performed += NorthFunc;
+        inputs.Movement.South.performed += SouthFunc;
+        inputs.Movement.West.performed += WestFunc;
+        inputs.Movement.East.performed += EastFunc;
+    }
+    public void NorthFunc(InputAction.CallbackContext ctx)
+    {
+        if(ctx.performed)
+        {
+        Debug.Log ("North input called.");
+
+        inputs.Movement.North.performed -= NorthFunc;
+
+        }
+
+    }
+    public void SouthFunc(InputAction.CallbackContext ctx)
+    {
+
+        Debug.Log ("South input Added.");
+    }
+    public void WestFunc(InputAction.CallbackContext ctx)
+    {
+
+        Debug.Log ("West input Added.");
+    }
+    public void EastFunc(InputAction.CallbackContext ctx)
+    {
+
+        Debug.Log ("East input Added.");
+    }
+}
